99c92bf9195692cfee04c219e8606f33688944e0
GyuReeKim <starkim96@naver.com>
2019-11-02 22:08:21 +0900

    character_detail

--- informations/urls.py
+++ informations/urls.py
@@ -5,8 +5,12 @@ app_name = "informations"
 
 urlpatterns = [
     path('', views.index, name="index"),
-    
+
     # character_list
     path('survivor/', views.survivor_list, name="survivor_list"),
     path('hunter/', views.hunter_list, name="hunter_list"),
+
+    # character_detail
+    path('survivor/<int:survivor_id>/', views.survivor_detail, name="survivor_detail"),
+    path('hunter/<int:hunter_id>/', views.hunter_detail, name="hunter_detail"),
 ]
--- informations/views.py
+++ informations/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Survivor, Hunter
 
 # Create your views here.
@@ -20,3 +20,19 @@ def hunter_list(request):
         'role': '감시자',
     }
     return render(request, 'informations/character_list.html', context)
+
+def survivor_detail(request, survivor_id):
+    character = get_object_or_404(Survivor, id=survivor_id)
+    context = {
+        'character': character,
+        'role': '생존자',
+    }
+    return render(request, 'informations/character_detail.html', context)
+
+def hunter_detail(request, hunter_id):
+    character = get_object_or_404(Hunter, id=hunter_id)
+    context = {
+        'character': character,
+        'role': '감시자',
+    }
+    return render(request, 'informations/character_detail.html', context)
