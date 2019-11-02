cec571d752236dcf0b68128c0d001b45ef37dafe
GyuReeKim <starkim96@naver.com>
2019-11-02 21:59:41 +0900

    character_list

--- informations/urls.py
+++ informations/urls.py
@@ -5,4 +5,8 @@ app_name = "informations"
 
 urlpatterns = [
     path('', views.index, name="index"),
+    
+    # character_list
+    path('survivor/', views.survivor_list, name="survivor_list"),
+    path('hunter/', views.hunter_list, name="hunter_list"),
 ]
--- informations/views.py
+++ informations/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import Survivor, Hunter
 
 # Create your views here.
 def index(request):
     return render(request, 'informations/index.html')
+
+def survivor_list(request):
+    characters = Survivor.objects.all()
+    context = {
+        'characters': characters,
+        'role' : '생존자',
+    }
+    return render(request, 'informations/character_list.html', context)
+
+def hunter_list(request):
+    characters = Hunter.objects.all()
+    context = {
+        'characters': characters,
+        'role': '감시자',
+    }
+    return render(request, 'informations/character_list.html', context)
