74900e5d887f4c2ce939b67f3f8c93a3ef29328d
GyuReeKim <starkim96@naver.com>
2019-11-02 21:54:41 +0900

    index

--- identity5/urls.py
+++ identity5/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from informations import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('informations/', include('informations.urls')),
+    path('', views.index),
 ]
