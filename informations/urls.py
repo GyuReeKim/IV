befad9d090f5cb75ee837f21f9dd2bc24403ab8e
GyuReeKim <starkim96@naver.com>
2019-11-02 22:19:14 +0900

    accounts form 추가 & character_like

--- informations/urls.py
+++ informations/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     # character_detail
     path('survivor/<int:survivor_id>/', views.survivor_detail, name="survivor_detail"),
     path('hunter/<int:hunter_id>/', views.hunter_detail, name="hunter_detail"),
+
+    # character_like
+    path('survivor/<int:survivor_id>/like/', views.survivor_like, name="survivor_like"),
+    path('hunter/<int:hunter_id>/like/', views.hunter_like, name="hunter_like"),
 ]
--- informations/views.py
+++ informations/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Survivor, Hunter
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def index(request):
@@ -36,3 +38,23 @@ def hunter_detail(request, hunter_id):
         'role': '감시자',
     }
     return render(request, 'informations/character_detail.html', context)
+
+@login_required
+def survivor_like(request, survivor_id):
+    survivor = get_object_or_404(Survivor, id=survivor_id)
+    user = request.user
+    if user in survivor.like_users.all():
+        survivor.like_users.remove(user)
+    else:
+        survivor.like_users.add(user)
+    return redirect('informations:survivor_detail', survivor_id)
+
+@login_required
+def hunter_like(request, hunter_id):
+    hunter = get_object_or_404(Hunter, id=hunter_id)
+    user = request.user
+    if user in hunter.like_users.all():
+        hunter.like_users.remove(user)
+    else:
+        hunter.like_users.add(user)
+    return redirect('informations:hunter_detail', hunter_id)
